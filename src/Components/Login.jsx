eb469e38288fc4fc8fe477d65df1e96ae86ebc56
nabeel <nabeelali720@gmail.com>
2024-04-24 22:40:55 +0500

    almost complete

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom'
 function Login() {
   return (
     <div>
-    <h1 className='text-3xl text-center underline'>Login</h1>
+    <h1 className='text-3xl text-center text-white underline'>Login</h1>
 <Link to='/home/assistants'>
-        <button className='text-2xl text-black text-center'>Go to Next Page</button>
+        <button className='text-2xl  text-center text-white'>Click Here</button>
 </Link>
     </div>
   )
