e4c5595dc0782a388f407e64ef63f41a0cce7f02
nabeel <nabeelali720@gmail.com>
2024-04-25 15:50:48 +0500

    updated

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,13 +1,64 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import { Icon } from '@iconify/react/dist/iconify.js'
 function Login() {
   return (
+
+    <div className="flex justify-center items-center h-screen">
+      <div className="bg-black  w-96 p-4 rounded-3xl">
+        <h1 className="text-white text-2xl text-center font-bold">Talkai247</h1>
+        <p className='text-white text-sm text-center py-2'>Welcome back! Please log into your account</p>
+        {/* <div className="flex items-center mt-4">
+
+          <input type="text" className="w-full p-2 rounded-full relative" placeholder="Email" />
+          <Icon icon="heroicons-solid:lock-open" className='absolute left-0 top-2' color='black' width="36" height="36" />
+        </div> */}
+         <div>
+
+      <div className="relative mt-2 rounded-md shadow-sm py-2">     
+        <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+        <Icon icon="fa-solid:user" color="black" width="22" height="22" />
+        
+                </div>
+        <input
+          type="text"
+
+          className="w-full rounded-3xl outline-none border border-gray-500 py-1.5 pl-14 px-12  text-gray-900  placeholder:text-gray-40 sm:text-sm sm:leading-6"
+          placeholder="Email"
+        />
+      
+      </div>
+    </div>
     <div>
-    <h1 className='text-3xl text-center text-white underline'>Login</h1>
-<Link to='/home/assistants'>
-        <button className='text-2xl  text-center text-white'>Click Here</button>
-</Link>
+      <div className="relative mt-2 rounded-md shadow-sm">     
+        <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+        <Icon icon="heroicons-solid:lock-open" width="22" color='black' height="22" />                </div>
+        <input
+          type="text"
+
+          className="w-full rounded-3xl outline-none border border-gray-500 py-1.5 pl-14 px-12  text-gray-900  placeholder:text-gray-40 sm:text-sm sm:leading-6"
+          placeholder="Password"
+        />
+      
+      </div>
+    </div>
+
+        <p className='text-white text-sm text-center py-2'>or continue with</p>
+        <button className="w-full mt-2 flex justify-center items-center gap-3 bg-orange-500 text-white py-1.5 px-2 rounded-full">
+        <Icon icon="eva:google-fill" width="30" height="30" />
+          
+          Sign in with Google</button>
+
+        <Link to='/home/assistants'> <button className="w-full mt-4 bg-[#55E2CA] text-white p-2 rounded-full"> 
+    Login
+    </button> </Link>
+
+
+        <p className='text-white text-sm text-center py-2 font-thin'>Forgot your <span className='font-semibold'>password</span></p>
+        <p className='text-white text-sm text-center py-2 font-thin'>Don't have an account? <span className='font-semibold text-green-400'> Sign up</span></p>
+
+
+      </div>
     </div>
   )
 }
--- src/Components/SidebarPages/Assistants.jsx
+++ src/Components/SidebarPages/Assistants.jsx
@@ -1,6 +1,8 @@
 import { Icon } from '@iconify/react/dist/iconify.js'
 import React from 'react'
-
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import "../../App.css"
 function Assistants() {
   return (
     <div>
@@ -59,7 +61,96 @@ function Assistants() {
 
 
 </div>
-<div className='w-full lg:w-[80%] border border-gray-500 rounded-md '>d</div>
+<div className='w-full lg:w-[80%] p-2 border bg-black border-gray-500 rounded-md '>
+
+{/* assistant nav content start */}
+
+<div className=' space-y-2'>
+<div className='w-full flex justify-between items-center md:flex-row flex-col bg-[#26292E] p-2'>
+<div className='flex '>
+
+<span className='text-white'>Ava</span>
+<span className='text-gray-500 ml-2'>49aecaf4-b1db</span>
+<div className='bg-black flex rounded-lg justify-center items-center p-1.5'>
+<Icon icon="fa-solid:copy" color='gray' width="18" height="18" />
+</div>
+</div>
+<div className='flex gap-1'>
+
+<div className='bg-black text-center rounded-lg flex justify-center items-center'>
+<Icon icon="fa-solid:share-alt" color='gray' width="26" height="26" className='p-1.5' />
+</div>
+<div className='bg-black text-center rounded-lg flex justify-center items-center'>
+<Icon icon="fluent-mdl2:location-dot" color='gray' width="28" height="28" className='p-1' />
+</div>
+
+<div className='bg-[#FE7E29] p-1 rounded-full flex items-center gap-1.5'>
+<div className='bg-black text-center rounded-full flex justify-center items-center'>
+<Icon icon="subway:call" color='#55E3CB' width="28" height="28" className='p-1.5' />
+</div>
+<h1>Test Ava</h1>
+</div>
+
+</div>
+</div>
+<div className='w-full flex justify-between items-center md:flex-row flex-col bg-[#26292E] p-2'>
+<div className='flex items-center gap-2'>
+
+<Icon icon="subway:menu" color='#171717' width="20" height="20" />
+<div className='p-1'>
+<span className='text-white mr-1'>Mode</span>
+<select name="" id="" className='bg-black text-white border border-gray-600 px-1 rounded-lg w-32'>
+  <option value="">Web</option>
+</select>
+</div>
+</div>
+<div className='flex gap-1'>
+
+<div className='bg-black p-1.5 rounded-full flex items-center gap-1.5'>
+<div className='bg-[#26292E] text-center rounded-full flex justify-center items-center'>
+<Icon icon="wpf:delete" fill='gray' color='#6C6C6C' width="28" height="28" className='p-1.5' />
+</div>
+<h1 className='text-[#BCBDBF]'>Discard</h1>
+</div>
+
+<div className='bg-[#FE7E29] p-1 rounded-full flex items-center gap-1.5'>
+<div className='bg-black text-center rounded-full flex justify-center items-center'>
+<Icon icon="oi:data-transfer-upload" color='#55E3CB' width="28" height="28" className='p-1.5' />
+</div>
+<h1>Publish</h1>
+</div>
+
+</div>
+</div>
+<div>
+
+</div>
+
+</div>
+{/* assistant nav content end */}
+
+
+<div className='p-2 border border-gray-600 '>
+<Tabs >
+    <TabList className="border-b-0  ">
+      <Tab >Voice</Tab>
+      <Tab>Title 2</Tab>
+    </TabList>
+
+    <TabPanel>
+      <h2>Any content 1</h2>
+    </TabPanel>
+    <TabPanel>
+      <h2>Any content 2</h2>
+    </TabPanel>
+  </Tabs>
+
+</div>
+
+
+
+
+</div>
 </div>
 
     </div>
